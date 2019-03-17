f00b87283a788a5366a0a87a4b37bd867180d168
Urinbasarov Dias <jentle.men29@gmail.com>
2019-03-17 18:46:18 +0600

    done

---
+++ HomeWork2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using NCalc;
+
+namespace HomeWork2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<string> historyList = new List<string>();
+        string hisoryItem;
+
+        private void GridClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            if (btn == null)
+                return;
+            string value = btn.Content.ToString();
+            if (string.IsNullOrEmpty(value))
+                return;
+            switch (value)
+            {
+                case "C":
+                    display.Text = "0";
+                    break;
+                case "=":
+                    try
+                    {
+                        NCalc.Expression exp = new NCalc.Expression(display.Text);
+                        hisoryItem = display.Text + value + exp.Evaluate().ToString();
+                        historyList.Add(hisoryItem);
+                        history.Text = "";
+                        for (int i = 0; i<historyList.Count; i++)
+                        {
+                            history.Text += historyList[i].ToString() + Environment.NewLine;
+                        }
+
+                        display.Text = exp.Evaluate().ToString();
+                    }
+                    catch
+                    {
+                        display.Text = "Error";
+                    }
+                    break;
+                default:
+                    display.Text = display.Text.TrimStart('0') + value;
+                    break;
+            }
+        }
+    }
+}
